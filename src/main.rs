0bad41a5a0ce50dc2aad75dfe14e8bb019396651
John Stilley <john_science@protonmail.com>
2022-10-23 08:35:03 -0700

    Implementing the basic game Object struct

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,49 @@ const LIMIT_FPS: i32 = 20;
 
 struct Tcod {
     root: Root,
+    con: Offscreen,
 }
 
 
-fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+/// This is a generic object: the player, a monster, an item, the stairs...
+/// It's always represented by a character on screen.
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    /// move by the given amount
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    /// set the color and then draw the character that represents this object at its position
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
     let key = tcod.root.wait_for_keypress(true);
     match key {
         // movement keys
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
 
         // Escape to exit game
         Key { code: Escape, .. } => return true,
@@ -32,7 +61,8 @@ fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool 
         _ => {}
     }
 
-    false
+    // NOTE: A language convenience says this line could just be "false", with no "return" or ";".
+    return false;
 }
 
 
@@ -48,22 +78,46 @@ fn main() {
         .title("February Second")
         .init();
 
-    // use the TCOD "Root" object to create a mutable TCOD struct
-    let mut tcod = Tcod { root };
+    // initialize an off-screen console
+    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    // player position
-    let mut player_x: i32 = SCREEN_WIDTH / 2;
-    let mut player_y: i32 = SCREEN_HEIGHT / 2;
+    // use the TCOD "Root" object to create a mutable TCOD struct
+    let mut tcod = Tcod { root , con };
+
+    // create object representing the player
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+
+    // create an NPC
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+
+    // the list of objects with those two
+    let mut objects = [player, npc];
 
     // the game loop!
     while !tcod.root.window_closed() {
-        tcod.root.set_default_foreground(WHITE);
-        tcod.root.clear();
-        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        // clear the off-screen console
+        tcod.con.clear();
+
+        // draw the player (and other objects) on an off-screen console
+        for object in &objects {
+            object.draw(&mut tcod.con);
+        }
+
+        // blit the player's off-screen console onto the root console
+        blit(
+            &tcod.con,
+            (0, 0),
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            &mut tcod.root,
+            (0, 0),
+            1.0,
+            1.0,
+        );
         tcod.root.flush();
 
         // handle keys and exit game if needed
-        let exit: bool = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, player);
         if exit { break; }
     }
 }

35299dd5f9c6487d4c75e6b0ce6e3c5d5ca9e116
John Stilley <john_science@protonmail.com>
2022-10-23 09:03:05 -0700

    Adding game map and immovable walls

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,18 @@ const SCREEN_HEIGHT: i32 = 50;
 // 20 frames-per-second maximum
 const LIMIT_FPS: i32 = 20;
 
+// size of the map
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+// colors for map objects
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+
 struct Tcod {
     root: Root,
     con: Offscreen,
@@ -29,10 +41,12 @@ impl Object {
         Object { x, y, char, color }
     }
 
-    /// move by the given amount
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    /// move by the given amount, if the destination is not blocked
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     /// set the color and then draw the character that represents this object at its position
@@ -43,17 +57,94 @@ impl Object {
 }
 
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
+/// A tile of the map and its properties
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+
+// NOTE: Alternatively, this could be a 1D vector of length HEIGHTxWIDTH
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+
+fn make_map() -> Map {
+    // fill map with "unblocked" tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // place two pillars to test the map
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    return map;
+}
+
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+    // go through all tiles, and set their background color
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    // draw all objects in the list
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+
+    // blit the contents of "con" to the root console
+    blit(
+        &tcod.con,
+        (0, 0),
+        (MAP_WIDTH, MAP_HEIGHT),
+        &mut tcod.root,
+        (0, 0),
+        1.0,
+        1.0,
+    );
+}
+
+
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
     let key = tcod.root.wait_for_keypress(true);
     match key {
         // movement keys
-        Key { code: Up, .. } => player.move_by(0, -1),
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, game),
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
 
         // Escape to exit game
         Key { code: Escape, .. } => return true,
@@ -78,8 +169,8 @@ fn main() {
         .title("February Second")
         .init();
 
-    // initialize an off-screen console
-    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    // initialize an off-screen console (for map, not menu)
+    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     // use the TCOD "Root" object to create a mutable TCOD struct
     let mut tcod = Tcod { root , con };
@@ -90,6 +181,12 @@ fn main() {
     // create an NPC
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
 
+    // make a Map of room objects
+    let game = Game {
+        // generate map (at this point it's not drawn to the screen)
+        map: make_map(),
+    };
+
     // the list of objects with those two
     let mut objects = [player, npc];
 
@@ -98,26 +195,14 @@ fn main() {
         // clear the off-screen console
         tcod.con.clear();
 
-        // draw the player (and other objects) on an off-screen console
-        for object in &objects {
-            object.draw(&mut tcod.con);
-        }
+        // draw the game screen
+        render_all(&mut tcod, &game, &objects);
 
-        // blit the player's off-screen console onto the root console
-        blit(
-            &tcod.con,
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root,
-            (0, 0),
-            1.0,
-            1.0,
-        );
         tcod.root.flush();
 
         // handle keys and exit game if needed
         let player = &mut objects[0];
-        let exit = handle_keys(&mut tcod, player);
+        let exit = handle_keys(&mut tcod, &game, player);
         if exit { break; }
     }
 }

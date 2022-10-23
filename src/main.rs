19b27df3d6b11556f4f601c630abd9883508b449
John Stilley <john_science@protonmail.com>
2022-10-22 18:29:01 -0700

    The player can move!

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,33 @@ struct Tcod {
     root: Root,
 }
 
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        // movement keys
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        // Escape to exit game
+        Key { code: Escape, .. } => return true,
+
+        _ => {}
+    }
+
+    false
+}
+
+
 fn main() {
+    // set the FPS
+    tcod::system::set_fps(LIMIT_FPS);
+
     // initialize the TCOD "Root" object
     let root: Root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
@@ -25,13 +51,20 @@ fn main() {
     // use the TCOD "Root" object to create a mutable TCOD struct
     let mut tcod = Tcod { root };
 
+    // player position
+    let mut player_x: i32 = SCREEN_WIDTH / 2;
+    let mut player_y: i32 = SCREEN_HEIGHT / 2;
+
     // the game loop!
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
+
+        // handle keys and exit game if needed
+        let exit: bool = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit { break; }
     }
 }
 

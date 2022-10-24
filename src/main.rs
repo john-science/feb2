003bbcc23cc797ba574363f692636b837b7e2d6e
John Stilley <john_science@protonmail.com>
2022-10-24 06:17:40 -0700

    Adding FOV using TCOD library

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-// TCOD imports
+/*
+February Second
+ */
+use rand::Rng;
+use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
-
-// import standard lib
-use std::cmp;
-
-// grab pseudo random number generating library
-use rand::Rng;
+use tcod::map::{FovAlgorithm, Map as FovMap};
 
 // actual size of the window
 const SCREEN_WIDTH: i32 = 80;
@@ -19,13 +18,16 @@ const LIMIT_FPS: i32 = 20;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 
+// field-of-view
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
+const FOV_LIGHT_WALLS: bool = true; // light walls or not
+const TORCH_RADIUS: i32 = 10;
+
 // colors for map objects
-const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
-const COLOR_DARK_GROUND: Color = Color {
-    r: 50,
-    g: 50,
-    b: 150,
-};
+const COLOR_DARK_WALL: Color = Color { r: 6, g: 3, b: 1 };
+const COLOR_DARK_GROUND: Color = Color { r: 81, g: 44, b: 15 };
+const COLOR_LIGHT_WALL: Color = Color { r: 30, g: 16, b: 5 };
+const COLOR_LIGHT_GROUND: Color = Color { r: 124, g: 65, b: 21 };
 
 //parameters for dungeon generator
 const ROOM_MAX_SIZE: i32 = 10;
@@ -36,6 +38,7 @@ const MAX_ROOMS: i32 = 30;
 struct Tcod {
     root: Root,
     con: Offscreen,
+    fov: FovMap,
 }
 
 
@@ -201,10 +204,10 @@ fn make_map(player: &mut Object) -> Map {
             } else {
                 // all rooms after the first:
                 // connect it to the previous room with a tunnel
-            
+
                 // center coordinates of the previous room
                 let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
-            
+
                 // toss a coin (random bool value -- either true or false)
                 if rand::random() {
                     // first move horizontally, then vertically
@@ -226,24 +229,37 @@ fn make_map(player: &mut Object) -> Map {
 }
 
 
-fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object], fov_recompute: bool) {
+    if fov_recompute {
+        // recompute FOV if needed (the player moved or something)
+        let player = &objects[0];
+        tcod.fov
+            .compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
+    }
+
     // go through all tiles, and set their background color
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
+            let visible = tcod.fov.is_in_fov(x, y);
             let wall = game.map[x as usize][y as usize].block_sight;
-            if wall {
-                tcod.con
-                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                tcod.con
-                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
-            }
+            let color = match (visible, wall) {
+                // outside of field of view:
+                (false, true) => COLOR_DARK_WALL,
+                (false, false) => COLOR_DARK_GROUND,
+                // inside fov:
+                (true, true) => COLOR_LIGHT_WALL,
+                (true, false) => COLOR_LIGHT_GROUND,
+            };
+            tcod.con
+                .set_char_background(x, y, color, BackgroundFlag::Set);
         }
     }
 
     // draw all objects in the list
     for object in objects {
-        object.draw(&mut tcod.con);
+        if tcod.fov.is_in_fov(object.x, object.y) {
+            object.draw(&mut tcod.con);
+        }
     }
 
     // blit the contents of "con" to the root console
@@ -294,11 +310,12 @@ fn main() {
         .title("February Second")
         .init();
 
-    // initialize an off-screen console (for map, not menu)
-    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
-
     // use the TCOD "Root" object to create a mutable TCOD struct
-    let mut tcod = Tcod { root , con };
+    let mut tcod = Tcod {
+        root,
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT),
+        fov: FovMap::new(MAP_WIDTH, MAP_HEIGHT),
+    };
 
     // create object representing the player
     let player = Object::new(0, 0, '@', WHITE);
@@ -306,7 +323,7 @@ fn main() {
     // create an NPC
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
 
-    // the list of objects with those two                                                                                                                                                                          
+    // the list of objects with those two
     let mut objects = [player, npc];
 
     // make a Map of room objects
@@ -315,18 +332,35 @@ fn main() {
         map: make_map(&mut objects[0]),
     };
 
+    // populate the FOV map, according to the generated map
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            tcod.fov.set(
+                x,
+                y,
+                !game.map[x as usize][y as usize].block_sight,
+                !game.map[x as usize][y as usize].blocked,
+            );
+        }
+    }
+
+    // force FOV "recompute" first time through the game loop
+    let mut previous_player_position = (-1, -1);
+
     // the game loop!
     while !tcod.root.window_closed() {
         // clear the off-screen console
         tcod.con.clear();
 
-        // draw the game screen
-        render_all(&mut tcod, &game, &objects);
+        // render the screen
+        let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+        render_all(&mut tcod, &game, &objects, fov_recompute);
 
         tcod.root.flush();
 
         // handle keys and exit game if needed
         let player = &mut objects[0];
+        previous_player_position = (player.x, player.y);
         let exit = handle_keys(&mut tcod, &game, player);
         if exit { break; }
     }

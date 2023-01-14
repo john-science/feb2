99583562d3873db96d9dd6075be9582eb7fbdce8
John Stilley <john_science@protonmail.com>
2023-01-14 13:09:07 -0800

    Cleaning up the Hello RLTK page

--- rltk/src/main.rs
+++ rltk/src/main.rs
@@ -1,17 +1,17 @@
-use rltk::{Rltk, GameState};
+use rltk::{Rltk, RltkBuilder, GameState};
 
 struct State {}
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+        ctx.print(1, 1, "You awaken in Purgatory.");
+        ctx.print(1, 2, "You mislived, you know why you're here.");
     }
 }
 
 fn main() -> rltk::BError {
-    use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
-        .with_title("Roguelike Tutorial")
+        .with_title("February Second")
         .build()?;
     let gs = State{ };
     rltk::main_loop(context, gs)

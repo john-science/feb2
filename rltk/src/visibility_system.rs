3af737ad7f58a36841bf565779a5b4efe49c02a4
John Stilley <john_science@protonmail.com>
2023-01-21 16:30:12 -0800

    Fog of War fully functional!

--- rltk/src/visibility_system.rs
+++ rltk/src/visibility_system.rs
@@ -1,22 +1,32 @@
 use specs::prelude::*;
-use super::{Viewshed, Position, Map};
+use super::{Viewshed, Position, Map, Player};
 use rltk::{field_of_view, Point};
 
 pub struct VisibilitySystem {}
 
 impl<'a> System<'a> for VisibilitySystem {
-    type SystemData = ( ReadExpect<'a, Map>,
+    type SystemData = ( WriteExpect<'a, Map>,
+                        Entities<'a>,
                         WriteStorage<'a, Viewshed>,
-                        WriteStorage<'a, Position>);
+                        WriteStorage<'a, Position>,
+                        ReadStorage<'a, Player>);
 
     fn run(&mut self, data : Self::SystemData) {
-        let (map, mut vs, pos) = data;
+        let (mut map, entities, mut viewshed, pos, player) = data;
 
-        for (vs, pos) in (&mut vs, &pos).join() {
-            vs.visible_tiles.clear();
-            // TODO: &*map ?  Why???
-            vs.visible_tiles = field_of_view(Point::new(pos.x, pos.y), vs.range, &*map);
-            vs.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height );
+        for (ent,viewshed,pos) in (&entities, &mut viewshed, &pos).join() {
+            viewshed.visible_tiles.clear();
+            viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
+            viewshed.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height );
+
+            // If this is the player, reveal what they can see
+            let p : Option<&Player> = player.get(ent);
+            if let Some(p) = p {
+                for vis in viewshed.visible_tiles.iter() {
+                    let idx = map.xy_idx(vis.x, vis.y);
+                    map.revealed_tiles[idx] = true;
+                }
+            }
         }
     }
 }

1fbe759dc971b538fb29df2309477f4649319441
John Stilley <john_science@protonmail.com>
2023-01-22 05:48:59 -0800

    Fullyi working Fog of War!

--- rltk/src/map.rs
+++ rltk/src/map.rs
@@ -13,7 +13,8 @@ pub struct Map {
     pub rooms : Vec<Rect>,
     pub width : i32,
     pub height : i32,
-    pub revealed_tiles : Vec<bool>
+    pub revealed_tiles : Vec<bool>,
+    pub visible_tiles : Vec<bool>
 }
 
 impl Map {
@@ -56,7 +57,8 @@ impl Map {
             rooms : Vec::new(),
             width : 80,
             height: 50,  // TODO: Magic numbers
-            revealed_tiles : vec![false; 80*50]
+            revealed_tiles : vec![false; 80*50],
+            visible_tiles : vec![false; 80*50]
         };
 
         const MAX_ROOMS : i32 = 30;
@@ -112,7 +114,6 @@ impl BaseMap for Map {
 
 
 // TODO: Colors should be const
-/// Not the best version
 pub fn draw_map(ecs: &World, ctx : &mut Rltk) {
     let map = ecs.fetch::<Map>();
 
@@ -120,15 +121,22 @@ pub fn draw_map(ecs: &World, ctx : &mut Rltk) {
     let mut x = 0;
     for (idx,tile) in map.tiles.iter().enumerate() {
         // Render a tile depending upon the tile type
+
         if map.revealed_tiles[idx] {
+            let glyph;
+            let mut fg;
             match tile {
                 TileType::Floor => {
-                    ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+                    glyph = rltk::to_cp437('.');
+                    fg = RGB::from_f32(0.0, 0.5, 0.5);
                 }
                 TileType::Wall => {
-                    ctx.set(x, y, RGB::from_f32(0.0, 1.0, 0.0), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+                    glyph = rltk::to_cp437('#');
+                    fg = RGB::from_f32(0., 1.0, 0.);
                 }
             }
+            if !map.visible_tiles[idx] { fg = fg.to_greyscale() }
+            ctx.set(x, y, fg, RGB::from_f32(0., 0., 0.), glyph);
         }
 
         // Move the coordinates

5b390e63387f827bcafdcea8482db03c32226486
John Stilley <john_science@protonmail.com>
2023-01-24 19:24:33 -0800

    Adding placeholder for monster AI

--- rltk/src/main.rs
+++ rltk/src/main.rs
@@ -3,25 +3,33 @@ use specs::prelude::*;
 
 mod components;
 mod map;
+mod monster_ai_system;
 mod player;
 mod rect;
 mod visibility_system;
 
 pub use components::*;
 pub use map::*;
+pub use monster_ai_system::MonsterAI;
 use player::*;
 pub use rect::Rect;
 use visibility_system::VisibilitySystem;
 
 
+#[derive(PartialEq, Copy, Clone)]
+pub enum RunState { Paused, Running }
+
 pub struct State {
-    ecs: World
+    ecs: World,
+    pub runstate : RunState
 }
 
 impl State {
     fn run_systems(&mut self) {
         let mut vis = VisibilitySystem{};
         vis.run_now(&self.ecs);
+        let mut mob = MonsterAI{};
+        mob.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -30,8 +38,12 @@ impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
 
-        player_input(self, ctx);
-        self.run_systems();
+        if self.runstate == RunState::Running {
+            self.run_systems();
+            self.runstate = RunState::Paused;
+        } else {
+            self.runstate = player_input(self, ctx);
+        }
 
         draw_map(&self.ecs, ctx);
 
@@ -53,27 +65,39 @@ fn main() -> rltk::BError {
         .build()?;
 
     let mut gs = State{
-        ecs: World::new()
+        ecs: World::new(),
+        runstate : RunState::Running
     };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();  // TODO: Is it possible in ECS to say there can be only one of these?
     gs.ecs.register::<Viewshed>();
+    gs.ecs.register::<Monster>();
 
     let map : Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
 
     // putting in some placeholder NPCs
+    let mut rng = rltk::RandomNumberGenerator::new();
     for room in map.rooms.iter().skip(1) {
         let (x,y) = room.center();
+
+        let glyph : rltk::FontCharType;
+        let roll = rng.roll_dice(1, 2);
+        match roll {
+            1 => { glyph = rltk::to_cp437('g') }
+            _ => { glyph = rltk::to_cp437('o') }
+        }
+
         gs.ecs.create_entity()
             .with(Position{ x, y })
             .with(Renderable{
-                glyph: rltk::to_cp437('g'),
+                glyph: glyph,
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
-            //.with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: false })
+            .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
+            .with(Monster{})
             .build();
     }
 

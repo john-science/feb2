ebf1dfc93931da5c880ad9a5c9ac7d8f3183650d
John Stilley <john_science@protonmail.com>
2023-01-30 06:43:12 -0800

    Moving NPC deaths to GUI log

--- rltk/src/damage_system.rs
+++ rltk/src/damage_system.rs
@@ -1,6 +1,6 @@
 use rltk::console;
 use specs::prelude::*;
-use super::{CombatStats, Player, SufferDamage};
+use super::{CombatStats, GameLog, Name, Player, SufferDamage};
 
 pub struct DamageSystem {}
 
@@ -25,12 +25,20 @@ pub fn delete_the_dead(ecs : &mut World) {
     {
         let combat_stats = ecs.read_storage::<CombatStats>();
         let players = ecs.read_storage::<Player>();
+        let names = ecs.read_storage::<Name>();
         let entities = ecs.entities();
+        let mut log = ecs.write_resource::<GameLog>();
         for (entity, stats) in (&entities, &combat_stats).join() {
-            if stats.hp < 1 { 
+            if stats.hp < 1 {
                 let player = players.get(entity);
                 match player {
-                    None => dead.push(entity),
+                    None => {
+                        let victim_name = names.get(entity);
+                        if let Some(victim_name) = victim_name {
+                            log.entries.push(format!("{} is dead", &victim_name.name));
+                        }
+                        dead.push(entity)
+                    }
                     Some(_) => console::log("You are dead")
                 }
             }
@@ -39,6 +47,6 @@ pub fn delete_the_dead(ecs : &mut World) {
 
     for victim in dead {
         ecs.delete_entity(victim).expect("Unable to delete");
-    }    
+    }
 }
 
